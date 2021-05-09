787626d9884088b2745e3262d043a03f59a2bcb8
Ltoroc97 <39684159+Ltoroc97@users.noreply.github.com>
2021-05-09 00:56:31 -0500

    Maqueta Contraseña
    Creación de Recuperar contraseña

--- View/RecuperarContrasena.php
+++ View/RecuperarContrasena.php
@@ -18,3 +18,56 @@
 	<!--Custom styles-->
 	<link rel="stylesheet" type="text/css" href="../Assets/Css/RecuperarContrasena.css">
 </head>
+
+<style type="text/css">
+	
+
+html,body{
+background-image: url('../Assets/Image/background_moto.jpeg');
+background-size: cover;
+background-repeat: no-repeat;
+height: 100%;
+font-family: 'Numans', sans-serif;
+}
+
+</style>
+
+<body>
+<div class="container">
+	<div class="d-flex justify-content-center h-100">
+
+		<div class="card">
+			
+			<div>
+				<img class="logo_formulario" src="../Assets/Image/irelogo.png"></img>
+			</div>
+				<div class="card-body card_formulario">
+				
+					<form>
+
+							<div class="input-group form-group">
+								<div class="input-group-prepend">
+										<span class="input-group-text"><i class="fas fa-user"></i></span>
+								</div>
+								<input type="text" class="form-control" placeholder="Número de Documento">
+							</div>
+
+							<div class="form-group ">
+								<input type="submit" value="Recuperar Contraseña" class="btn login_btn boton_formulario_ingreso">
+							</div>
+						</form>
+
+						<br>
+
+											
+							<div class="d-flex justify-content-center links">
+								<a href="../View/InicioSesion.php">
+									<p class="btn-group-lg">Cancelar</p>
+								</a>
+							</div>
+				</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
